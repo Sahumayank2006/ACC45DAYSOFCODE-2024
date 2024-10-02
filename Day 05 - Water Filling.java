58dc44c11f8749aa764192bbd08fb4f314db70ae
Sahumayank2006 <mayank.sahu1@s.amity.edu>
2024-10-02 21:46:34 +0530

    Create Day 05 - Water Filling.java
    Day 05 - Water filling (Easy)
    Done on 02 October 2024

---
+++ Day 05 - Water Filling.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class WaterFilling
+{
+	public static void main (String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int T=sc.nextInt();
+	    for(int i=1;i<=T;i++){
+	    int B1=sc.nextInt();
+int B2=sc.nextInt();
+int B3=sc.nextInt();
+if (B1==0 && B2==0 || B1==0 && B3==0 || B2==0 && B3==0)
+    System.out.println("Water filling time");
+    else
+    System.out.println("Not now");
+
+
+	    }
+	}
+}
