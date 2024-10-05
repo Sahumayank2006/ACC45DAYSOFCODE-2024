76be8d02a8d1f7f88b279998ffe327ae04b1dea2
Sahumayank2006 <mayank.sahu1@s.amity.edu>
2024-10-05 06:51:48 +0530

    Create Day 07- Janmansh and Assignments.java
    Day 07-Janmansh and Assignments (EASY)
    done on 04 October 2024

---
+++ Day 07- Janmansh and Assignments.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	int T=sc.nextInt();// Enter number of test Cases
+	for(int i=1;i<=T;i++){
+	    int X=sc.nextInt();
+	    if((X+3)<=10)//test condition
+	    System.out.println("Yes");
+	    else
+	    	    System.out.println("No");
+	}
+
+	}
+}
