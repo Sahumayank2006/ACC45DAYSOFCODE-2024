7fd77e0513df8eb9a3ac1bf93ab2449a278df69c
Sahumayank2006 <mayank.sahu1@s.amity.edu>
2024-10-11 17:16:43 +0530

    Create DAY 14 - Maximise The Tastiness.java
     DAY 14 - Maximise The Tastiness
    Done on 11 October 2024

---
+++ DAY 14 - Maximise The Tastiness.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class MaximisetheTastiness {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();//input the number of test cases
+        for(int i=1;i<=T;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            int d=sc.nextInt();
+            int sum=0;
+            if(a>b){
+            sum=sum+a;
+            }
+            else{
+            sum=sum+b;
+            }
+             if(c>d){
+            sum=sum+c;
+             }
+            else{
+            sum=sum+d;
+            }
+            System.out.println(sum);
+        }
+    }
+}
