958f41b20241fbcae65093377bf235443246464e
Sahumayank2006 <mayank.sahu1@s.amity.edu>
2024-11-10 23:01:21 +0530

    Create Day 39 -Cyclic Quadrilateral.java

---
+++ Day 39 -Cyclic Quadrilateral.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class CyclicQuadrilateral
+{
+	public static void main (String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	int T=sc.nextInt();
+	for(int i=0;i<T;i++){
+	    int A=sc.nextInt();
+	    int B=sc.nextInt();
+	    int C=sc.nextInt();
+	    int D=sc.nextInt();
+	    if((A+C)==180 && (B+D)==180)
+	    System.out.println("YES");
+	    else
+	     System.out.println("No");
+	}
+	}
+}
