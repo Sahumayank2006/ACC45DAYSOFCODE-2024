1c0f282446b569011ef4e3d7840a80ea30abce8e
Sahumayank2006 <mayank.sahu1@s.amity.edu>
2024-11-15 20:12:35 +0530

    Update Day 44 -  Card Swipe.java

--- Day 44 -  Card Swipe.java
+++ Day 44 -  Card Swipe.java
@@ -7,27 +7,26 @@ import java.util.*;
 
         while (T-- > 0) {
             int N = sc.nextInt();
-            int[] swipeIds = new int[N];
+            int[] card = new int[N];
             for (int i = 0; i < N; i++) {
-                swipeIds[i] = sc.nextInt();
+                card[i] = sc.nextInt();
             }
 
-            
-            HashSet<Integer> insideSet = new HashSet<>();
+  HashSet<Integer> insideSet = new HashSet<>();
             int currentInside = 0;
             int maxInside = 0;
 
-            for (int id : swipeIds) {
+            for (int id : card) {
                 if (insideSet.contains(id)) {
-                    // If the ID is already in the set, it's an out-swipe
-                    insideSet.remove(id);
+                    
+                   insideSet.remove(id);
                     currentInside--;
                 } else {
-                    // If the ID is not in the set, it's an in-swipe
+                    
                     insideSet.add(id);
                     currentInside++;
                 }
-                // Update the maximum count of people inside
+                
                 maxInside = Math.max(maxInside, currentInside);
             }
 
